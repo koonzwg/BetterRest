ac5dfd51693d303e77a33b55571da0d7c0f9dbf7
koonzwg <koonzwg@gmail.com>
2024-03-17 15:33:08 -0500

    Day 28 complete

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -10,11 +10,8 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var wakeUp = defaultWakeTime
-    @State private var sleepAmount = 8.0
+    @State private var sleepAmount = 8
     @State private var coffeeAmount = 1
-    @State private var alertTitle = ""
-    @State private var alertMessage = ""
-    @State private var showingAlert = false
     
     static var defaultWakeTime: Date {
         var components = DateComponents()
@@ -23,67 +20,53 @@ struct ContentView: View {
         return Calendar.current.date(from: components) ?? .now
     }
     
-    func calculateBedtime() {
+    var calculateBedtime: String {
         do {
             let config = MLModelConfiguration()
             let model = try SleepCalculator(configuration: config)
-            
             let components = Calendar.current.dateComponents([.hour, .minute], from: wakeUp)
             let hour = (components.hour ?? 0) * 60 * 60
             let minute = (components.minute ?? 0) * 60
-            
-            let prediction = try model.prediction(wake: Double(hour + minute), estimatedSleep: sleepAmount, coffee: Double(coffeeAmount))
-            
+            let prediction = try model.prediction(wake: Double(hour + minute), estimatedSleep: Double(sleepAmount), coffee: Double(coffeeAmount))
             let sleepTime = wakeUp - prediction.actualSleep
-            
-            alertTitle = "Your ideal bedtime is..."
-            alertMessage = sleepTime.formatted(date: .omitted, time: .shortened)
+            return sleepTime.formatted(date: .omitted, time: .shortened)
         } catch {
-            alertTitle = "Error"
-            alertMessage = "Sorry, there was a problem calculating your bedtime."
+            return "Error calculating bedtime"
         }
-        
-        showingAlert = true
     }
     
     var body: some View {
         NavigationStack {
             Form {
-                VStack(alignment: .leading, spacing: 8) {
-                    Text("When do you want to wake up?")
-                        .font(.headline)
+                Section("When do you want to wake up?") {
                     DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
                         .labelsHidden()
                 }
                 
-                
-                VStack(alignment: .leading, spacing: 8) {
-                    Text("Desired amount of sleep")
-                        .font(.headline)
-                    Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
+                Section("Desired amount of sleep") {
+                    Picker("Desired amount of sleep", selection: $sleepAmount) {
+                        ForEach(4..<13) { amount in
+                            Text("\(amount) hours")
+                        }
+                    }
+                    .pickerStyle(.automatic)
                 }
                 
-                
-                VStack(alignment: .leading, spacing: 8) {
-                    Text("Daily coffee intake")
-                        .font(.headline)
-                    Stepper("^[\(coffeeAmount) cup](inflect: true)", value: $coffeeAmount, in: 1...20)
+                Section("Daily coffee intake") {
+                    Picker("Daily coffee intake", selection: $coffeeAmount) {
+                        ForEach(1...20, id: \.self) { amount in
+                            Text(amount == 1 ? "\(amount) cup" : "\(amount) cups")
+                        }
+                    }
+                    .pickerStyle(.automatic)
                 }
                 
-            }
-            .alert(alertTitle, isPresented: $showingAlert) {
-                Button("OK") {}
-            } message: {
-                Text(alertMessage)
+                Section("Recommended Bedtime") {
+                    Text(calculateBedtime)
+                        .font(.largeTitle)
+                }
             }
             .navigationTitle("BetterRest")
-            .toolbar {
-                Button("Calculate", action: calculateBedtime)
-                
-            }
-            
-            
-            
         }
     }
 }
