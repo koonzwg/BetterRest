3c8562c43b0fd8fbdd8864ac2ef98b5bbaf19e23
koonzwg <koonzwg@gmail.com>
2024-03-17 15:09:30 -0500

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by William Koonz on 3/17/24.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
